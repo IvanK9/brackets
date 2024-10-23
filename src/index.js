20f2cde84a70ad3ab5d1b9b262b12fdebebb2252
Ivan <ivankonovalov96@inbox.ru>
2024-10-23 21:18:47 +0500

    feat: add solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let stack = [];
+
+  if (str.length % 2 > 0) return false;
+
+  for (let i = 0; i < str.length; i++) {
+
+    let currentSym = str[i];
+    let topStack = stack[stack.length - 1];
+
+    bracketsConfig.forEach(function (item) {
+      if (currentSym === item[0] && !stack.includes(item[1])) {
+        stack.push(currentSym);
+      } else if (stack.length === 0) {
+        return false;
+      } else if (currentSym === item[1] && item.includes(topStack)) {
+        stack.pop();
+      }
+    });
+  }
+  return stack.length === 0;
+};
